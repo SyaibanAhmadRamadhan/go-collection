29464c0b17c82844638d45712ac56d01609b00a2
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2024-10-19 22:40:47 +0700

    update stack trace

--- error.go
+++ error.go
@@ -1,59 +1,16 @@
 package collection
 
 import (
-	"errors"
 	"fmt"
 	"runtime"
 )
 
-type StackErr struct {
-	stackErr error
-
-	// opt using http status code
-	httpCode int
-}
-
-func (s *StackErr) Error() string {
-	return s.stackErr.Error()
-}
-
-func (s *StackErr) Unwrap() error {
-	currentErr := s.stackErr
-	var lastErr error
-	for {
-		currentErr = errors.Unwrap(currentErr)
-		if currentErr == nil {
-			return lastErr
-		}
-
-		lastErr = currentErr
-	}
-}
-
-func (s *StackErr) GetHttpCode() int {
-	return s.httpCode
-}
-
-type Option func(s *StackErr)
-
-func SetHttpCode(code int) Option {
-	return func(s *StackErr) {
-		s.httpCode = code
-	}
-}
-
-func Err(err error, opts ...Option) *StackErr {
+func Err(err error) error {
 	pc := make([]uintptr, 15)
 	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
 	frame, _ := frames.Next()
-	s := &StackErr{
-		stackErr: fmt.Errorf("%s:%d: %w", frame.Function, frame.Line, err),
-	}
-	for _, opt := range opts {
-		opt(s)
-	}
-	return s
+	return fmt.Errorf("%s:%d: %w", frame.Function, frame.Line, err)
 }
 
 func PanicIfErr(err error) {
